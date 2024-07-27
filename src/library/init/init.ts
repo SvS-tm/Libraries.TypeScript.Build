04becd34e2b87a262c8619c9f331d9eb056135b2
SvS-NickM <svs.nickm@outlook.com>
2024-07-28 00:09:03 +0300

    fix: fixed globs & init

--- src/library/init/init.ts
+++ src/library/init/init.ts
@@ -1,24 +1,29 @@
 import { existsSync } from "fs";
 import { copyFile, mkdir } from "fs/promises";
 import { glob } from "glob";
-import { basename, join } from "path";
+import { dirname, join } from "path";
 
 export async function init()
 {
-    const buildAssets = await glob("tsconfig.*", { cwd: __dirname, absolute: true, nodir: true, posix: true });
-
-    if (!existsSync("build"))
-        await mkdir("build");
+    const buildAssets = await glob("**", { cwd: join(__dirname, "init"), absolute: false, nodir: true, posix: true, dot: true });
 
     await Promise.all
     (
         buildAssets.map
         (
-            (asset) => copyFile
-            (
-                asset,
-                join("build", basename(asset))
-            )
+            async (asset) => 
+            {
+                const directory = dirname(asset);
+
+                if (!existsSync(directory))
+                    await mkdir(directory, { recursive: true });
+            
+                await copyFile
+                (
+                    join(__dirname, asset),
+                    asset
+                );
+            }
         )
     );
 }
