04a626b4f95c1bab911a56f3c6d8279d179e975d
SvS-NickM <svs.nickm@outlook.com>
2024-07-28 16:04:25 +0300

    fix: init command fixed
    - pack fixed

--- build/pack.ts
+++ build/pack.ts
@@ -1,6 +1,6 @@
 import { execSync } from "child_process";
 import { existsSync } from "fs";
-import { copyFile, mkdir, writeFile } from "fs/promises";
+import { copyFile, mkdir, rm, rmdir, writeFile } from "fs/promises";
 import { glob } from "glob";
 import { basename, dirname, join } from "path";
 
@@ -19,6 +19,41 @@ type ReleaseManifest =
 
 async function pack()
 {
+    if (existsSync("bin"))
+        await rm("bin", { force: true, recursive: true });
+
+    await mkdir("bin");
+
+    const intermediateResults = await glob("**", { absolute: false, cwd: "obj", posix: true, nodir: true, dot: true });
+
+    await Promise.all
+    (
+        intermediateResults.map
+        (
+            async (path) => 
+            {
+                const source = join("obj", path);
+                const destination = join("bin", path);
+
+                const destinationDirectory = dirname(destination);
+
+                if (!existsSync(destinationDirectory))
+                    await mkdir(destinationDirectory, { recursive: true });
+
+                await copyFile(source, destination);
+            }
+        )
+    );
+
+    const npmignore = [
+        ".release-artifact/",
+        "*.tgz",
+        "!.npmrc",
+        ""
+    ];
+
+    await writeFile("bin/.npmignore", npmignore.join("\n"), { encoding: "utf-8", flag: "w" });
+
     const assets = await glob("**", { absolute: false, cwd: "assets", posix: true, nodir: true, dot: true });
 
     await Promise.all
